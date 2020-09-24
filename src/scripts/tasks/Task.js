fbb9ec36431b0fb7495dfbd8d059ab04e9d56176
Megan McCoy <mnmccoy95@gmail.com>
2020-09-24 12:29:02 -0500

    added task save function

--- src/scripts/tasks/Task.js
+++ src/scripts/tasks/Task.js
@@ -1,9 +1,11 @@
 export const Task = (taskObj) => {
     if(taskObj.status === false){
-        return `<input type="checkbox" id="${taskObj.id}" name="task${taskObj.id}" value="${taskObj.task}">
-        <label for="task${taskObj.id}">${taskObj.task}</label><br>`
+        return `<input type="checkbox" id="check--${taskObj.id}" name="task${taskObj.id}" value="${taskObj.task}">
+        <label for="task${taskObj.id}">${taskObj.task}</label><button id="deleteTask--${taskObj.id}">Delete</button>
+        <button id="editTask--${taskObj.id}">Edit</button>`
     } else{
-        return `<input type="checkbox" id="${taskObj.id}" name="task${taskObj.id}" value="${taskObj.task}" checked>
-        <label for="task${taskObj.id}">${taskObj.task}</label><br>`
+        return `<input type="checkbox" id="check--${taskObj.id}" name="task${taskObj.id}" value="${taskObj.task}" checked>
+        <label for="task${taskObj.id}">${taskObj.task}</label><button id="deleteTask--${taskObj.id}">Delete</button>
+        <button id="editTask--${taskObj.id}">Edit</button>`
     }
 }
--- src/scripts/tasks/TaskList.js
+++ src/scripts/tasks/TaskList.js
@@ -2,21 +2,58 @@ import {getTasks, useTasks, saveTask, deleteTask} from "./TaskProvider.js"
 import {Task} from "./Task.js"
 
 const eventHub = document.querySelector(".container")
+const userId = sessionStorage.getItem("activeUser")
 
 export const renderTasksInitial = () => {
-    eventHub.innerHTML += `<div class="tasks"></div>`
+    eventHub.innerHTML += `<div class="tasks">
+        <div class="task-form"></div>
+        <div class="task-list"></div>
+    </div>`
     getTasks()
     .then(useTasks)
     .then(()=> {
         const myTasks = useTasks()
+        renderTaskForm()
         render(myTasks)
     })
 }
 
 const render = (tasks) => {
-    const taskTarget = document.querySelector(".tasks")
-
-
+    const taskTarget = document.querySelector(".task-list")
+    let taskListHTML = tasks.map(task => {
+        return Task(task)
+    }).join("<br>")
+    taskTarget.innerHTML = taskListHTML
 }
 
-const 
+const renderTaskForm = () => {
+    const taskTarget = document.querySelector(".task-form")
+    taskTarget.innerHTML += `<div>New Task</div>
+        <textarea id="task-text" placeholder="Enter task info...</textarea>
+        <div id="expectedCompletionDate">placeholder for calendar</div>
+        <button id="saveTask">Save New Task</button>
+    `
+}
+
+eventHub.addEventListener("click", e => {
+    if(e.target.id === "saveTask"){
+        const taskContent = document.querySelector("#task-text")
+        const taskDate = document.querySelector("#expectedCompletionDate")
+        if(taskContent.value !== "0" && taskDate.value !== "0"){
+            const newTask = {
+                userId: userId,
+                task: taskContent.value,
+                expectedCompletionDate: taskDate.value,
+                status: false
+            }
+            saveTask(newTask)
+        } else{
+            window.alert("Input New Task")
+        }
+
+    } else if(e.target.id.startsWith("deleteTask--")){
+
+    } else if(e.target.id.startsWith("editTask--")){
+
+    }
+})
