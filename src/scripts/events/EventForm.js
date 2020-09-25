fbb7b8bfc51eddda84deb91848d18e3dbdd9a740
Brady Logan <brady.drew.logan@gmail.com>
2020-09-25 11:12:21 -0400

    created saveEvent and setup EventForm

---
+++ src/scripts/events/EventForm.js
@@ -0,0 +1,11 @@
+import { saveEvent } from './EventsList.js'
+
+const contentTarget = document.querySelector('.events-form')
+
+const render = () => {
+
+}
+
+export const EventForm = () => {
+    
+}
--- src/scripts/events/EventsProvider.js
+++ src/scripts/events/EventsProvider.js
@@ -1,5 +1,7 @@
+//variable that holds the events
 let events = []
 
+//fetch events from the database using the given userId
 export const getEvents = (user) => {
     return fetch(`http://localhost:8088/events?userId=${user}`)
     .then(response => response.json())
@@ -8,6 +10,27 @@ export const getEvents = (user) => {
     })
 }
 
+//return the array of events
 export const useEvents = () => {
     return events.slice()
+}
+
+const eventHub = document.querySelector(".container")
+
+const dispatchEventChangeEvent = () => {
+    const eventStateChangedEvent = new CustomEvent("eventStateChanged")
+
+    eventHub.dispatchEvent(eventStateChangedEvent)
+}
+
+export const saveEvent = event => {
+    return fetch('http://localhost:8088/events', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(event)
+    })
+    .then(getEvents)
+    .then(dispatchEventChangeEvent)
 }
