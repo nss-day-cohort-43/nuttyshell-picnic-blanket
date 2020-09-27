1e5bd626831d23ea146b3db56bba2deb21ec32e1
Megan McCoy <mnmccoy95@gmail.com>
2020-09-26 19:46:17 -0500

    current weather renders

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -1,11 +1,13 @@
 import {publicMessagesStarter} from './publicMessages/MessageBox.js'
 import {renderTasksInitial} from "./tasks/TaskList.js"
 import {renderArticlesInitial} from "./articles/ArticleList.js"
+import {renderWeatherInitial} from './weather/Weather.js'
 
 export const Nutshell = () => {
     // Render all your UI components here
     //like renderTasks, renderArticles, renderEvents, etc
     //import render for all components here
+    renderWeatherInitial()
     publicMessagesStarter()
     renderTasksInitial()
     renderArticlesInitial()
