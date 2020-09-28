dca2b99822a9aa6f5b922ca877b11939c51f1420
bcdavis <bcollinsdavis@gmail.com>
2020-09-28 15:33:48 -0500

    definitely broken, help

--- src/scripts/friends/FriendList.js
+++ src/scripts/friends/FriendList.js
@@ -1,10 +1,11 @@
 import { FriendHTML } from "./Friend.js"
-import { getFriends, useFriends, getUsers, useUsers, saveFriend } from "./FriendProvider.js"
+import { getFriends, useFriends, getUsers, useUsers, saveFriend, deleteFriend } from "./FriendProvider.js"
 
 const eventHub = document.querySelector(".container");
-const activeUser = sessionStorage.getItem("activeUser"); // get the active user
+const userId = sessionStorage.getItem("activeUser"); // get the active user
 
-let validUsers = [];
+let validUsers = [];    // array of all valid users
+let friendsArray = [];  // store array of all friend objects for friends of the current user
 
 // renders current user's friends when first opening Nutshell
 export const renderFriendsInitial = () => {
@@ -20,8 +21,8 @@ export const renderFriendsInitial = () => {
     `
     getFriends() // fetch all of the current user's friends
     .then(() => {
-        const friends = useFriends();
-        makeFriendList(friends);
+        friendsArray = useFriends();
+        makeFriendList(friendsArray);
     })
     .then(() => {
         // fetch usernames of all valid users and store them in an array 
@@ -34,6 +35,7 @@ const fetchValidUsernames = () => {
     getUsers()
     .then(() => {
         validUsers = useUsers();
+        console.log(validUsers);
     })
 }
 
@@ -58,7 +60,8 @@ const renderAddFriendForm = () => {
     return `
         <div class="addFriend-form">
             <input type="text" class="friend-search-box" placeholder="Enter a username" name="usernameSearch">
-            <button type="button" id="saveFriendBtn">Save Friend</button>
+            <p class="invalid-username-warning"></p>
+            <button type="button" id="saveFriend-btn">Save Friend</button>
         </div>
     `;
 }
@@ -74,51 +77,135 @@ const renderAddFriendForm = () => {
 const toggleAddFriendForm = () => {
     const contentTarget = document.getElementById("addFriendByUsername")
     const formTarget = document.querySelector(".addFriend-form")
+    const addFriendBtnTarget = document.getElementById("addNewFriend-btn")
 
     if(formTarget){
         // If the addFriendForm exists, remove it
         contentTarget.innerHTML = "";
+        //lighten the "addNewFriend-btn" button
+        addFriendBtnTarget.setAttribute("style", "background-color:rgb(239, 239, 239); border-width:1px; border-radius:2px;");
     }
     else {
         // If the addFriendForm does not exist, render it
         contentTarget.innerHTML += `${renderAddFriendForm()}`;
+        // darken the "addNewFriend-btn" button
+        addFriendBtnTarget.setAttribute("style", "background-color:lightgrey; border-width:1px; border-radius:2px;");
     }
 
 }
+
+const deleteFriends2 = (idA, idB) => {
+    return deleteFriend(idA, idB) // delete friendship between friend and user
+        .then(getFriends)
+        .then(() => {
+            friendsArray = useFriends()
+            console.log(friendsArray);
+        })
+}
+
+// This event listener listens for clicks of the:
+//  - "addNewFriend-btn" button
+//  - "saveFriend-btn" button
+//  - "deleteFriend-btn" button
 
 eventHub.addEventListener("click", event => {
     if(event.target.id === "addNewFriend-btn"){
         toggleAddFriendForm(); // open or close the add friend form when "add new friend" button is pressed
     }
-    else if(event.target.id === "saveFriendBtn"){
+    else if(event.target.id.startsWith("deleteFriend-btn--")){
+        const [prefix, id] = event.target.id.split("--");
+        console.log(prefix);
+        console.log(id); // the id of the friend we want to delete
+        //debugger;
+        deleteFriends2(id, userId);
+    }
+    else if(event.target.id === "saveFriend-btn"){
         const inputUsername = document.querySelector(".friend-search-box");
-        // check if username is entered
-        if(inputUsername.input !== ""){
-            // If username entered is valid, send the friend a friend request and close the form
+        const warningTarget = document.querySelector(".invalid-username-warning");
+        console.log("inputUsername.value: ", inputUsername.value);
+        const trimmedInput = inputUsername.value.trim();
+        // If username entered is not an empty string
+        if(trimmedInput !== ""){
 
+            // Check if username entered is valid
             // Use a .find() to find the specific username entered by the current user
             // --------- EACH USERNAME SHOULD BE UNIQUE! ---------
             let targetUser = validUsers.find(user => {
-                return user === inputUsername.input;
+                return user.username === trimmedInput; // returns the matching user object
             })
             console.log("targetUser: ", targetUser);
 
-            // If a valid username was entered, add the friend
+            // If a valid username was entered, and
+            // If username entered is NOT the same as current user's
             if(targetUser){
-                saveFriend(targetUser);     // save new friend
-                inputUsername.input = "";   // clear input
-                toggleAddFriendForm();      // close addFriend form
+                if(targetUser.id !== parseInt(userId)){
+
+                    // Is username entered = any of current user's existing friends?
+                    if(friendsArray.find(friend => friend.user.username === targetUser.username)){
+                        console.log("Error: Already friends with this person");
+                        warningTarget.innerHTML = `You and this person are already friends!`;
+                    }
+                    else{
+                        // The username is not the current user's or one of the current user's friends
+                        // The username is also valid and not blank
+                        // Add the friend
+
+                        // Create two new friend objects representing the friendship between 
+                        // the current user and the username they entered and saved
+
+                        // TODO: Implement a friend request system
+
+                        // "id" of each friend object is auto-updated upon POST
+                        const friendship_currentUserToFriend = {
+                            userId: parseInt(userId),
+                            friendId: targetUser.id,
+                            accepted: true
+                        };
+
+                        const friendship_friendToCurrentUser = {
+                            userId: targetUser.id,
+                            friendId: parseInt(userId),
+                            accepted: true
+                        };
+
+                        warningTarget.innerHTML = "";                   // clear any present warning message
+                        saveFriend(friendship_currentUserToFriend);     // save new friend
+                        saveFriend(friendship_friendToCurrentUser);     // save new friend
+                        inputUsername.input = "";                       // clear input
+                        toggleAddFriendForm();                          // close addFriend form
+                    }
+                }
+                else{
+                    warningTarget.innerHTML = `Cannot add yourself as a friend`;
+                }
             }
             // If an invalid username was entered, display warning and keep input
-            else if(targetUser == undefined){
-                const warningTarget = document
+            else if(!targetUser){
+                console.log("Username is invalid, 1", trimmedInput);
+                warningTarget.innerHTML = `${trimmedInput} is not a valid username`;
             }
-
-
         }
+        // If no username entered, show warning of empty input
         else {
-            // No username entered, do nothing
+            console.log("Username is blank, 2", trimmedInput);
+            inputUsername.input = ""; // clear input
+            inputUsername.value = ""; // clear input
+            inputUsername.textContent = ""; // clear input
+            warningTarget.innerHTML = `Please type another user's username`;
 
         }
     }
+})
+
+eventHub.addEventListener("friendStateChanged", event => {
+    // Friend State updated, refresh friends
+    getFriends() // fetch all of the current user's friends
+    .then(() => {
+        friendsArray = useFriends();
+        makeFriendList(friendsArray);
+    })
+    .then(() => {
+        // fetch usernames of all valid users and store them in an array 
+        fetchValidUsernames(); 
+    })
 })
--- src/scripts/friends/FriendProvider.js
+++ src/scripts/friends/FriendProvider.js
@@ -1,6 +1,8 @@
 let friends = [];
 let users = [];
 
+let friendship = [];
+
 
 const eventHub = document.querySelector(".container");
 // Let the applicaiton know the friend object state was changed
@@ -10,12 +12,14 @@ const dispatchFriendStateChangeEvent = () => {
     eventHub.dispatchEvent(friendStateChangedEvent)
 }
 
+// --------------- Get all friends of current user -----------------
+
 export const getFriends = () => {
-    const usrId = sessionStorage.getItem("activeUser")
+    const userId = sessionStorage.getItem("activeUser")
 
     // Fetch the friends of the current user
     // where the userId = friendId -- get all users that are friends with that friendId
-    return fetch(`http://localhost:8088/friends?_expand=user&friendId=${usrId}`) 
+    return fetch(`http://localhost:8088/friends?_expand=user&friendId=${userId}`) 
     .then(response => response.json())
     .then(
         parsedRelationships => { // parsedRelationships is an array of objects!
@@ -26,21 +30,39 @@ export const getFriends = () => {
 
 export const useFriends = () => friends.slice();
 
+// -------------------- Get just one friendship ------------------
+
+export const getFriendship = (friendId) => {
+    const userId = sessionStorage.getItem("activeUser");
+    return fetch(`http://localhost:8088/friends?&friendId=${friendId}&userId=${userId}`)
+    .then(response => response.json())
+    .then(
+        parsedFriendAToB => {
+            friendship[0] = parsedFriendAToB
+        }
+    )
+    .then(
+        fetch(`http://localhost:8088/friends?&friendId=${userId}&userId=${friendId}`)
+        .then(response => response.json())
+        .then(
+            parsedFriendBToA => {
+                friendship[1] = parsedFriendBToA
+            }
+        )
+    )
+}
+
+export const useFriendship = () => friendship.slice();
+
+// ------------------------ Get all users ----------------------------
+
 export const getUsers = () => {
     return fetch(`http://localhost:8088/users?`)
     .then(response => response.json())
     .then(
         parsedUsers => { // parsedUsers is an array of user objects
             console.log("ParsedUsers: ", parsedUsers);
-            // return an array of just the uernames
-            let usernames = parsedUsers.map(user => {
-                console.log("user.username: ", user.username);
-                if(user.username !== ""){
-                    // If the user has a username of at least one character
-                    return user.username
-                }
-            });
-            users = usernames; // only get the usernames of each user
+            users = parsedUsers; // array of user objects
             console.log("users: ", users);
         }
     )
@@ -48,7 +70,9 @@ export const getUsers = () => {
 
 export const useUsers = () => users.slice();
 
-// save the added friend to the friends object in the api
+
+
+// ---------- Save the added friend to the friends object in the api
 export const saveFriend = (friend) => {
     return fetch('http://localhost:8088/friends', {
         method: "POST",
@@ -56,6 +80,15 @@ export const saveFriend = (friend) => {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(friend)
+    })
+    .then(getFriends)
+    .then(dispatchFriendStateChangeEvent)
+}
+
+// ----------- Delete a selected friend from your friends in the api
+export const deleteFriend = (id1, id2) => {
+    return fetch(`http://localhost:8088/friends?&friendId=${id1}&userId=${id2}`, {
+        method: "DELETE",
     })
     .then(getFriends)
     .then(dispatchFriendStateChangeEvent)
