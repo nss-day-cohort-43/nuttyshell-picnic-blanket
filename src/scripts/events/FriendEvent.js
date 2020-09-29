eb2fd4cbf2db4d3dca69313c209d95fd96808446
Brady Logan <brady.drew.logan@gmail.com>
2020-09-29 15:45:10 -0400

    cleaned up code

--- src/scripts/events/FriendEvent.js
+++ src/scripts/events/FriendEvent.js
@@ -4,10 +4,9 @@ export const FriendEvent = (event) => {
     return `
         <div id="${event.startDate}--${event.id}" class="event-card event-card-${event.id}">
             <h3 id="event-name-${event.id}">${event.name}</h3>
-            <h4 id="event-owner-${event.id}">Owner:${event.user.username}</h4>
+            <h4 id="event-owner-${event.id}">Owner: ${event.user.username}</h4>
             <div id="event-date-${event.id}">Date: ${event.startDate}</div>
             <div id="event-location-${event.id}">Location: ${event.eventCity}, ${event.eventState} ${event.eventZip}</div>
         </div>
     `
-
 }
