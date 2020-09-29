57d1f8286ac2fc6468cc4bc1fbc80fc1007071de
Brady Logan <brady.drew.logan@gmail.com>
2020-09-29 14:04:27 -0400

    now displays friends events

--- src/scripts/events/FriendEventsList.js
+++ src/scripts/events/FriendEventsList.js
@@ -5,6 +5,12 @@ export const FriendEventsList = () => {
     getFriendsEvents()
     .then(_ => {
         const friendsEvents = useFriendsEvents()
-        debugger;
+        const contentTarget = document.querySelector('.friends-events-list')
+        contentTarget.innerHTML = `
+            <h3>Friends Events</h3>
+            ${friendsEvents.map(event => {
+                return FriendEvent(event)
+            })}
+        `
     })
 }
